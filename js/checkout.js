f1b646c8c3b014f9b8c41dc3a12f3df37057eee5
Gil Sala <gilsala_88@hotmail.com>
2023-02-04 20:33:26 +0100

    Add styles and validation paramaters to the checkout form

--- js/checkout.js
+++ js/checkout.js
@@ -1,28 +1,86 @@
 
 // Exercise 6
 function validate() {
-	var error = 0;
+	let error = 0;
 	// Get the input fields
-	var fName = document.getElementById("fName");
-	var fEmail = document.getElementById("fEmail");
+	const fName = document.getElementById("fName");
+	const fEmail = document.getElementById("fEmail");
+	const fAddress = document.getElementById("fAddress");
+	const fLastN = document.getElementById("fLastN");
+	const fPassword = document.getElementById("fPassword")
+	const fPhone = document.getElementById("fPhone")
 
 	// Get the error elements
-	var errorName = document.getElementById("errorName");
-	var errorEmail = document.getElementById("errorEmail");  
-	
+	const errorName = document.getElementById("errorName");
+	const errorEmail = document.getElementById("errorEmail");
+	const errorAddress = document.getElementById("errorAddress");
+	const errorLastN = document.getElementById("errorLastN");
+	const errorPassword = document.getElementById("errorPassword");
+	const errorPhone = document.getElementById("errorPhone");
+
+
 	// Validate fields entered by the user: name, phone, password, and email
-	if(fName.value == ""){
+	const checkNames = /^[A-Za-z]+$/;
+	const checkEmail = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+	const checkPassword = /^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/
+	const checkPhone = /^\d+$/
+
+
+	if (fName.value == "" || fName.value.length < 3 || !fName.value.match(checkNames)) {
+		errorName.style.display = "block"
+		fName.style.border="thick solid #EA4B4B"
 		error++;
+	} else {
+		errorName.style.display = "none"
 	}
 
-	if(fEmail.value == ""){
+	if (fLastN.value == "" || fLastN.value.length < 3 || !fName.value.match(checkNames)) {
 		error++;
+		errorLastN.style.display = "block"
+		fLastN.style.border="thick solid #EA4B4B"
+	} else {
+		errorLastN.style.display = "none"
 	}
-	 
-	if(error>0){
-		alert("Error");
+
+	if (fEmail.value == "" || fEmail.value.length < 3 || !fEmail.value.match(checkEmail)) {
+		error++;
+		errorEmail.style.display = "block"
+		fEmail.style.border="thick solid #EA4B4B"
 	}else{
+		errorEmail.style.display = "none"	
+	}
+
+	if (fAddress == "" || fAddress.value.length < 3) {
+		error++
+		errorAddress.style.display = "block"
+		fAdress.style.border="thick solid #EA4B4B"
+	}else{
+		errorAddress.style.display = "none"
+	}
+
+	if (fPassword == "" || fPassword.value.length < 3 || !fPassword.value.match(checkPassword)) {
+		error++;
+		errorPassword.style.display = "block"
+		fPassword.style.border="thick solid #EA4B4B"
+	}else{
+		errorPassword.style.display = "none"
+	}
+
+	if (fPhone == "" || fPhone.value.length < 9 || !fPhone.value.match(checkPhone)) {
+		errorPhone.style.display = "block"
+		fPhone.style.border="thick solid #EA4B4B"
+		error++;
+	}else{
+		errorPhone.style.display = "none"
+
+	}
+
+	if (error > 0) {
+		alert("Error");
+	} else {
 		alert("OK");
 	}
 
 }
+
+
